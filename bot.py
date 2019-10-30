59a482fa66a2586263c91e3a182437838b30739e
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-10-30 09:08:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,8 +44,5 @@ while True:
     response = s.recv(1024).decode("utf-8")
     if response == "PING :tmi.twitch.tv\r\n":
         s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-    else:
-        username = re.search(r"\w+", line).group(0) # return the entire match
-        message = CHAT_MSG.sub("", line)
-        print(username + ": " + message)
+   
     sleep(1 / cfg.RATE)
