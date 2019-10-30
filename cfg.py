7300b586fd65900039c2c1ed3dcc5cb77b8106a7
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-10-30 08:59:54 +0100

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -5,7 +5,7 @@ token = chatbot_token.token
 
 HOST = "irc.twitch.tv"              # the Twitch IRC server
 PORT = 6667                         # always use port 6667!
-NICK = "twitch_username"            # your Twitch username, lowercase
+NICK = "bud_lan_b"                  # your Twitch username, lowercase
 PASS = token                        # your Twitch OAuth token
 CHAN = "#bud_lan_b"                 # the channel you want to join
 RATE = (20/30)                      # messages per second
