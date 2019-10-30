33c954b3a65e8cb0d1978f9e7386e9767757977c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-10-30 10:56:56 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,12 +3,12 @@
 #documentation: https://github.com/joshuaskelly/twitch-observer
 import time
 from twitchobserver import Observer
-import chatbot_token # imports local file with bot token
-token = chatbot_token.token
+import chatbot_token # imports local file with bot token (.gitignored, so create chatbot_token.py locally!). get twitch oauth from https://twitchapps.com/tmi/
+
 channel = 'bud_lan_b'
 nick = 'bud_lan_b'
 
-with Observer(nick, token) as observer:
+with Observer(nick, chatbot_token.token) as observer:
     observer.join_channel(channel)
     observer.send_message('Hallo. Ich bin ein Chat-Bot. !commands zeigt alle meine Befehle. Tommy stinkt übrigens!', channel)
 
