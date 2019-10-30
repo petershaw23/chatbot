c6c1e17d2887961329a740e8e81ecb06026e266a
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-10-30 08:48:12 +0100

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import chatbot_token # imports local file with bot token
+token = chatbot_token.token
+
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = "twitch_username"            # your Twitch username, lowercase
+PASS = token                        # your Twitch OAuth token
+CHAN = "#bud_lan_b"                 # the channel you want to join
